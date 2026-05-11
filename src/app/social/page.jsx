8a54da65f07034959bfd3f9070b6f4c0d07b8a84
sheikhmhummadhasan <hasan2001onlin@gmail.com>
2026-05-11 13:11:56 +0600

    remove banner photo

--- src/app/social/page.jsx
+++ src/app/social/page.jsx
@@ -5,20 +5,6 @@ const SocialWorkPage = () => {
         <div>
             <div className="">
                 <div className="">
-                    <div className="relative h-[25vh] sm:h-[30vh] md:h-[40vh] w-full bg-[url('/happy-doc.avif')] bg-cover bg-center bg-no-repeat">
-
-                        {/* Overlay */}
-                        <div className="absolute inset-0 bg-black/50"></div>
-
-                        {/* Content */}
-                        <div className="relative z-10 flex items-center justify-center h-full px-4">
-                            <h1 className="text-white text-2xl sm:text-3xl md:text-5xl font-bold text-center">
-                                Meet Our Specialist Doctor
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="">
                     <div className="w-11/12 max-w-7xl mx-auto py-10">
 
                         {/* White Container */}
